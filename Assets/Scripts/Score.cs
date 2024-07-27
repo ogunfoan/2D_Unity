5c1d5d66df22f67e18258d5ec1c9a04c6daede00
ogunfoan <64503396+ogunfoan@users.noreply.github.com>
2024-07-27 15:44:47 +0300

    better scoring system

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Score : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _yazialani;
     private AudioSource _audio; //ses kaynağını tanımlıyoruz.
 
     private void Awake() // sistem ayağa kalktığında yapılacaklar
     {
         _audio = GetComponent<AudioSource>(); //audioSource dan gelen sesi al
+        _yazialani.text = TotalScore.Tscore.ToString();
     }
 
     private void OnTriggerEnter2D(Collider2D other) // bizim objemiz başka bir objeyle dokundukları anda
@@ -18,8 +20,9 @@ public class Score : MonoBehaviour
         {
             _audio.Play(); // olduğunda da sesi çal
             Destroy(other.gameObject); //öbür objeyi yok et
-            TScore++; //puanı arttır
-            
+            TotalScore.Tscore++; //puanı arttır
+            _yazialani.text = TotalScore.Tscore.ToString();
+
         }
     }
     
