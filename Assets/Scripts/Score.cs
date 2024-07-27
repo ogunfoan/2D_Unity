f109b08a3a52c5209c4c85b61cbe0d6d86bdc9d8
ogunfoan <64503396+ogunfoan@users.noreply.github.com>
2024-07-27 03:21:26 +0300

    Update Score.cs

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,4 +14,5 @@ public class Score : MonoBehaviour
             pointsa++;
         }
     }
+    
 }
