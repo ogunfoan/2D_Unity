2ce3d25b6e76f84165f0d02c2d051dd0e1586bc8
ogunfoan <64503396+ogunfoan@users.noreply.github.com>
2024-07-27 17:27:04 +0300

    v0.0.1
    can mekaniği tam çalışıyor
    zeminde aşağı düşmemizi engelleyen ve canımızı azaltan engel konuldu

--- Assets/Scripts/LivesControl.cs
+++ Assets/Scripts/LivesControl.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LivesControl : MonoBehaviour
 {
@@ -17,6 +18,9 @@ public class LivesControl : MonoBehaviour
             case 1:
                 gameObject.transform.GetChild(2).gameObject.SetActive(false);
                 gameObject.transform.GetChild(1).gameObject.SetActive(false);
+                break;
+            case 0:
+                SceneManager.LoadScene(4);
                 break;
             default:
                 break;
